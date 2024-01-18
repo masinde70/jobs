4d69917bcedf2a6e3b0cb559a114f54b92eee073
masinde70 <5812481+masinde70@users.noreply.github.com>
2024-01-18 10:46:41 +0200

    state management

--- states/advanced_state.py
+++ states/advanced_state.py
@@ -1,2 +1,17 @@
 import streamlit as st
 from datetime import datetime, timedelta
+
+st.title("Advance state Management")
+
+# Store widget value in session state
+st.subheader("Store widget value in session state")
+
+# Initialize widget value with session state
+st.subheader("Initialize widget value with session state")
+
+# Callbacks
+st.subheader("use callbacks")
+
+st.markdown("### select your time range")
+
+st.radio("select a range", ["7 days", "28 days", "custom"], horizontal=True)
