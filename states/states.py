3ac7f70fec8ab8d7cf63b509fc474894dfd2b47a
masinde70 <5812481+masinde70@users.noreply.github.com>
2024-01-29 14:58:00 +0200

    session states

--- states/states.py
+++ states/states.py
@@ -8,3 +8,16 @@ st.title("States")
 number = st.slider("A number", 1, 10, key="slider")
 
 st.write(st.session_state)
+
+col1, buff, col2 = st.columns([1, .5, 3])
+option_names = ["a", "b", "c"]
+
+option = col1.radio("Pick an option", option_names, key="radio_option")
+st.session_state
+
+if option == "a":
+    col2.write("You picked 'a' :smile:")
+elif option == "b":
+    col2.write("You picked 'b' :heart:")
+else:
+    col2.write("You picked 'c' :sunglasses:")
